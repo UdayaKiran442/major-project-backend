9a62fdc3cf8094945951b02745822f8d39a70505
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-30 17:28:36 +0530

    changes in email token schema

--- models/emailToken.js
+++ models/emailToken.js
@@ -5,6 +5,14 @@ const emailTokenSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  token: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 600,
+  },
 });
 
 const Token = mongoose.model("Token", emailTokenSchema);
