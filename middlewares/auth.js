164a741f9d61c5f4328111b0b7e8879ea2de11b0
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-02-27 09:50:03 +0530

    changes w.r.t auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/student");
-const { errorResponse, serverError } = require("../config/apiResponses");
+const { serverError } = require("../config/apiResponses");
+const { decodedToken } = require("../config/decodedToken");
 
 exports.isAuthenticated = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split("Bearer ")[1];
-    if (!token) {
-      return errorResponse(req, res, 401, "Login to perform an action");
-    }
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = decodedToken();
     req.user = await User.findById(decoded._id);
     next();
   } catch (error) {
