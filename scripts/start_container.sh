499b818b04b9019b188eb372c7f4d74b5dac364e
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2024-01-18 11:43:56 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull udayagonuguntla/campus-management-node-apis
+docker pull udayagonuguntla/ci-cd-demo
 
 # Run the Docker image as a container
-docker run -d -p 3000:3000 udayagonuguntla/campus-management-node-apis
+docker run -d -p 3000:3000 udayagonuguntla/ci-cd-demo
